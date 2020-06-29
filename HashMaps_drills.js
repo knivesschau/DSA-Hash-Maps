72554754d2f454c8f59766487a1a4e5cc865d66e
Catrina Fuentes <cat.fuentes93@gmail.com>
2020-06-28 22:18:14 -0700

    main function added

--- HashMaps_drills.js
+++ HashMaps_drills.js
@@ -1,2 +1,25 @@
 const HashMap = require('./HashMap');
 
+MAX_LOAD_RATIO = 0.5;
+SIZE_RATIO = 3;
+
+function main() {
+    const lotr = new HashMap();
+
+    lotr.set("Hobbit", "Bilbo");
+    lotr.set("Hobbit", "Frodo");
+    lotr.set("Wizard", "Gandalf");
+    lotr.set("Human", "Aragorn");
+    lotr.set("Elf", "Legolas");
+    lotr.set("Maiar", "The Necromancer");
+    lotr.set("Maiar", "Sauron");
+    lotr.set("RingBearer", "Gollum");
+    lotr.set("LadyOfLight", "Galadriel");
+    lotr.set("HalfElven", "Arwen");
+    lotr.set("Ent", "Treebeard");
+
+    
+
+}
+
+main();
